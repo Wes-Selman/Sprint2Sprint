e3b355137e2524dbe0f6e32162bc6eccfa0fba08
Wes-Selman <selman.wes@gmail.com>
2023-12-26 10:13:28 -0600

    Club Activity Stream V1

--- src/pages/YourDistance.js
+++ src/pages/YourDistance.js
@@ -20,3 +20,38 @@ const mapStateToProps = (state) => {
 };
 
 export default connect(mapStateToProps)(YourDistance);
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const ClubActivities = () => {
+  const [activities, setActivities] = useState([]);
+  const [error, setError] = useState(null);
+  const clubId = 1200758; // Club ID
+
+  useEffect(() => {
+    const fetchActivities = async () => {
+      try {
+        const response = await axios.get(`https://www.strava.com/api/v3/clubs/${clubId}/activities`, {
+          headers: { Authorization: `Bearer ${accessToken}` },
+          params: { // Optional query parameters
+            after: '2023-12-20', // Filter activities after this date
+            before: '2023-12-26', // Filter activities before this date
+          },
+        });
+
+        setActivities(response.data);
+      } catch (error) {
+        setError(error);
+      }
+    };
+
+    fetchActivities();
+  }, []);
+
+  // ... use the activities data to render your component
+
+  return (
+    // ... your component's JSX
+  );
+};
